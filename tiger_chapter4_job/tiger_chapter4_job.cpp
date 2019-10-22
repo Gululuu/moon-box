fd49435e1a8e3cdc9c8f4018eed480fc48cb2bda
仙仔 <@LAPTOP-J12QOQ1C>
2019-10-22 18:46:17 +0800

    添加项目文件。

---
+++ tiger_chapter4_job/tiger_chapter4_job.cpp
@@ -0,0 +1,26 @@
+// tiger_chapter4_job.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+int ch43_caculate_sum()
+{
+	int i = 0;
+
+	return 0;
+}
+
+int ch46_guess_num()
+{
+	int i = 0;
+
+	return 0;
+}
+
+int main()
+{
+	//ch43_caculate_sum();
+	ch46_guess_num();
+    return 0;
+}
+
